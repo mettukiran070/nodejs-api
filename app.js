51cf3ea66436c2006c4ee2518946cb480fe4d77b
kiran.mettu <kiran.mettu@predera.com>
2023-10-10 19:14:48 +0530

    Tested working as per expected

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose")
 const userRouter = require("./routes/UserRoutes");
 
 const app = express();
 const PORT = 3001;
 
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use("/api/users", userRouter);
+
+mongoose.connect(
+    "mongodb://root:example@localhost:27017",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("connected successfully"))
+  .catch((err) => console.log("unable to connect :: ", err))
 
 app.listen(PORT, () => console.log("Server is running on "+ PORT));
 
